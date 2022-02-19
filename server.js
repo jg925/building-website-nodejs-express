8ac95516ecc19facba03245e121abb78adda0d8d
jg925 <jg925@cornell.edu>
2022-02-18 20:43:11 -0500

    add error handling

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
+const createError = require('http-errors');
 
 const routes = require('./routes');
 const FeedbackService = require('./services/FeedbackService');
 const SpeakersService = require('./services/SpeakerService');
+const { create } = require('domain');
 
 const feedbackService = new FeedbackService('./data/feedback.json');
 const speakersService = new SpeakersService('./data/speakers.json');
@@ -27,6 +29,14 @@ app.set('views', path.join(__dirname, './views'));
 app.locals.siteName = 'ROUX Meetups';
 
 app.use(express.static(path.join(__dirname, './static')));
+
+// Async and Syncronized throw error example
+/* app.get('/throw', (req, res, next) => {
+  setTimeout(() => {
+    return next(new Error('Something threw'));
+  }, 500);
+}); */
+
 app.use(async (req, res, next) => {
   try {
     const names = await speakersService.getNames();
@@ -45,6 +55,20 @@ app.use(
     speakersService,
   })
 );
+
+app.use((req, res, next) => {
+  return next(createError(404, 'File not found.'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  console.error(err);
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.status(status);
+  res.render('error');
+});
 
 app.listen(port, () => {
   console.log(`express server listening on port ${port}!`);
